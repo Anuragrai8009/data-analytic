7be53d5933cb80e0558310c912e57e8594158cc7
Anuragrai8009 <raianurag8081@gmail.com>
2024-07-20 12:50:50 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,33 @@
+
+def div(a, b):
+    return a / b
+result = div(10, 2)
+print(result)  
+
+
+
+print("<--------------------------------------------------->")
+# Declare a square() function with one parameter.Then call the function and pass one number and display the square of that number .
+def square(x):
+    return x * x
+a = float(input("enter the number:-"))
+square_result = square(a)
+print(square_result)
+
+print("<--------------------------------------------------->")
+
+# Using max() and min() functions display the maximum and minimum of 5 random numbers
+
+num =[2,3,454,4,6]
+print (num)
+print(max(num))
+print(min(num))
+
+# Accept a name from the user and display that in lower case using lower() function
+name =(input("enter the name:-"))
+a= name.lower()
+print(a)
+
+# Write a Python program to count the occurrences of each word in a given sentance
+
+
